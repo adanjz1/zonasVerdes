cbeefc2ddb249839ef0722e7befe26d5d209aab3
mouremh <mouremh@gmail.com>
2014-08-19 02:21:15 -0300

    Estado(ok)Permisos(ok)
    Usuarios Permisos, mañana lo cambio, y en cuanto al crud, hice andar el
    json con join, pero me falta pulirlo cun el update y el Delete.

---
+++ application/controllers/Estado.php
@@ -0,0 +1,65 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Estado
+ *
+ * @author MM
+ */
+class Estado extends CI_Controller {
+    public function index()
+	{
+            $this->load->model('usuarios_model');
+            $data['permisions'] = $this->usuarios_model->getPermissions($_SESSION['usuario']->id);
+            $this->load->library('parser');
+            $this->parser->parse('widgets/header', $data);
+            $this->parser->parse('widgets/menu', $data);
+            $this->parser->parse('widgets/footer', $data);
+        }
+        public function lista(){
+            $this->load->library('crud');
+            $data = $this->crud->getDataList('Estado',$this);
+            
+            $this->load->model('usuarios_model');
+            $data['permisions'] = $this->usuarios_model->getPermissions($_SESSION['usuario']->id);
+            
+            $this->load->library('parser');
+            $this->parser->parse('widgets/header', $data);
+            $this->parser->parse('widgets/menu', $data);
+            $this->parser->parse('list', $data);
+            $this->parser->parse('widgets/footer', $data);
+        }
+        public function add($id=''){
+            $this->load->library('crud');
+            $data = $this->crud->getFormData('Estado',$this,$id);
+            
+            $this->load->model('usuarios_model');
+            $data['permisions'] = $this->usuarios_model->getPermissions($_SESSION['usuario']->id);
+            
+            $this->load->library('parser');
+            $this->parser->parse('widgets/header', $data);
+            $this->parser->parse('widgets/menu', $data);
+            $this->parser->parse('form', $data);
+            $this->parser->parse('widgets/footer', $data);
+        }
+        public function save(){
+            if(empty($_POST['id'])){
+                $this->load->model('crud_model');
+                $this->crud_model->insert($_POST,'Estado');
+            }else{
+                $this->load->model('crud_model');
+                $this->crud_model->update($_POST,'Estado');
+            }
+            redirect('Estado/lista');
+        }
+        public function delete($id){
+            $this->load->model('crud_model');
+            $this->crud_model->delete($id,'Estado');
+            redirect('Estado/lista');
+        }
+}
--- application/libraries/crud.php
+++ application/libraries/crud.php
@@ -22,6 +22,26 @@ class Crud {
             $d['table'] = $t->crud_model->select($select, $json->from,$json->where);
             return $d;
         }
+        public function getDataListJoin($file,$t){
+            $json = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/application/config/modulos/'.$file.'.json');
+            $json = json_decode($json);
+            $d = array();
+            $d['title'] = $json->title;
+            $d['module'] = $file;
+            $sep = '';
+            $d['rows'] = array();
+            $select ='';
+            foreach($json->fields as $f){
+                if($f->showList){
+                    $select .= $sep.$f->tableName.'.'.$f->fieldName;
+                    $sep = ',';
+                    $d['rows'][] = $f->fieldName;
+                }
+            }
+            $t->load->model('crud_model');
+            $d['table'] = $t->crud_model->select($select, $json->from,$json->where);
+            return $d;
+        }
         public function getFormData($file,$t,$id){
             $json = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/application/config/modulos/'.$file.'.json');
             $json = json_decode($json);
@@ -32,6 +52,26 @@ class Crud {
             if(!empty($id)){
                 $d['title'] = $json->titleM;
                 $where  = 'id = '.$id;
+                $t->load->model('crud_model');
+                $info = $t->crud_model->select('', $file,$where);
+                
+                $d['info'] = (array)$info[0];
+            }
+            $d['form'] = (array)$json->fields;
+            
+            return $d;
+        }
+         public function getFormDataJoin($file,$t,$id){
+            $json = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/application/config/modulos/'.$file.'.json');
+            $json = json_decode($json);
+            $d = array();
+            $d['title'] = $json->titleA;
+            $d['module'] = $file;
+            $d['info'] = '';
+            if(!empty($id)){
+                $d['title'] = $json->titleM;
+                $f=$json->fields;
+                $where  = $file.'id\' = '.$id;
                 $t->load->model('crud_model');
                 $info = $t->crud_model->select('', $file,$where);
                 
