2a3f3fdaab6f5fd93fdd89afc680097986347da9
adanjz1 <adanzweig@gmail.com>
2014-09-03 16:57:03 -0700

    Categorias y tareas
    Falta el tema de estados de la tarea

---
+++ application/controllers/Categoria.php
@@ -0,0 +1,73 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Categoria extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+            $this->load->model('usuarios_model');
+            $data['permisions'] = $this->usuarios_model->getPermissions($_SESSION['usuario']->id);
+            $this->load->library('parser');
+            $this->parser->parse('widgets/header', $data);
+            $this->parser->parse('widgets/menu', $data);
+            $this->parser->parse('widgets/footer', $data);
+        }   
+        public function lista(){
+            $this->load->library('crud');
+            $data = $this->crud->getDataList('Categoria',$this);
+            
+            $this->load->model('usuarios_model');
+            $data['permisions'] = $this->usuarios_model->getPermissions($_SESSION['usuario']->id);
+            
+            $data['nombreJson'] = "Categoria";
+            $this->load->library('parser');
+            $this->parser->parse('widgets/header', $data);
+            $this->parser->parse('widgets/menu', $data);
+            $this->parser->parse('list', $data);
+            $this->parser->parse('widgets/footer', $data);
+        }
+        public function add($id=''){
+            $this->load->library('crud');
+            $data = $this->crud->getFormData('Categoria',$this,$id);
+            
+            $this->load->model('usuarios_model');
+            $data['permisions'] = $this->usuarios_model->getPermissions($_SESSION['usuario']->id);
+            
+            $this->load->library('parser');
+            $this->parser->parse('widgets/header', $data);
+            $this->parser->parse('widgets/menu', $data);
+            $this->parser->parse('form', $data);
+            $this->parser->parse('widgets/footer', $data);
+        }
+        public function save(){
+            if(empty($_POST['id'])){
+                $this->load->model('crud_model');
+                $this->crud_model->insert($_POST,'Categoria');
+            }else{
+                $this->load->model('crud_model');
+                $this->crud_model->update($_POST,'Categoria');
+            }
+            redirect('Categoria/lista');
+        }
+        public function delete($id){
+            $this->load->model('crud_model');
+            $this->crud_model->delete($id,'Categoria');
+            redirect('Categoria/lista');
+        }
+}
+
+
