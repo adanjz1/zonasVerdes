518ef0ee276660305e5d601427b02a6933599602
adanjz1 <adanzweig@gmail.com>
2014-10-23 02:11:58 -0700

    Parte del upload de trabajo
    Necesita revision de datos, que datos mas hay q sacar de aca?, las
    funciones como "getComunafromValue" aun no estan creadas pero fijate si
    se necesita otra funcion pedorra de esas asi las hago

--- application/controllers/Trabajo.php
+++ application/controllers/Trabajo.php
@@ -35,8 +35,10 @@ class Trabajo extends CI_Controller {
             
             $data['nombreJson'] = "Trabajo";
             $this->load->library('parser');
+            $this->load->helper('form');
             $this->parser->parse('widgets/header', $data);
             $this->parser->parse('widgets/menu', $data);
+            $this->parser->parse('widgets/fileuploader', $data);
             $this->parser->parse('listAlta', $data);
             $this->parser->parse('widgets/footer', $data);
         }
@@ -70,6 +72,55 @@ class Trabajo extends CI_Controller {
             $this->crud_model->delete($id,'Trabajo');
             saveLog($this,'Trabajo borrado. ID:'.json_encode($_POST));
             redirect('Trabajo/lista');
+        }
+        public function altaTrabajos(){
+            	$this->load->helper('form');
+        	//Configure
+		//set the path where the files uploaded will be copied. NOTE if using linux, set the folder to permission 777
+		$config['upload_path'] = 'application/uploads/';
+                // set the filter image types
+		$config['allowed_types'] = 'xls';
+//		$config['file_name'] = 'paapas.csv';
+		//$config['file_ext'] = 'csv';
+		//load the upload library
+		$this->load->library('upload', $config);
+                
+//                $this->upload->set_filename();
+                $this->upload->initialize($config);
+                
+                $this->upload->set_allowed_types('*');
+                
+
+		$data['upload_data'] = '';
+    
+		//if not successful, set the error message
+		if (!$this->upload->do_upload('userfile')) {
+			$data = array('msg' => $this->upload->display_errors());
+                        die($data);
+		} else { //else, set the success message
+                        $data = $this->upload->data();
+                        $d = (file_get_contents($data['full_path']));
+                        $filas = (explode("\n",$d));
+                        foreach($filas as $f){
+                            $casillas = explode("\t",$f);
+                            var_dump($casillas);
+                            var_dump('--------------------------------');
+                            echo '<br>';
+                            if(!empty($casillas[0]) && !empty($casillas[26])){
+                                $arrayDatos = array(
+                                    'Observacion'=>$casillas[10],
+                                    'comuna'=> getComunafromValue(str_replace("C","",$casillas[22])),
+                                    'Calle'=>getCalleFrom($casillas[1]),
+                                    'Altura'=>getAlturaFrom($casillas[1]),
+                                    'ResolucionLegal'=> '',//ver cual es
+                                    'Especie'=>'',//cual es?
+                                );
+                            }
+                        }
+                }
+                        
+                        //$var = array('filename'=>$data['full_path'],'user'=>$_SESSION['user'],'platform'=>$_POST['platform'],'appname'=>$_POST['appname']);
+                        //$lastId = $this->report->saveReport($var);
         }
 }
 
---
+++ application/views/widgets/fileuploader.php
@@ -0,0 +1,11 @@
+
+<?php echo form_open_multipart('Trabajo/altaTrabajos');?>
+<div>
+    <div class="panel panel-info"><div class="panel-heading">Importar Trabajos</div>
+        <div class="panel-body" style="text-align: right;">
+            <input type="file" class="form-group" name="userfile" size="20" style="display:inline"/>
+            <button type="submit" class="btn btn-primary glyphicon glyphicon-open">Importar</button>
+        </div>
+    </div>
+</form>
+        
